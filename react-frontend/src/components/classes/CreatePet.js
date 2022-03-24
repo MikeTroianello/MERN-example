b9224b1e3cfe36adc711e79acded760c48ec2635
miketroianello <mike@Mikes-MacBook-Pro.local>
2022-03-24 19:42:12 -0400

    heroku

--- react-frontend/src/components/classes/CreatePet.js
+++ react-frontend/src/components/classes/CreatePet.js
@@ -1,84 +1,172 @@
-import React from "react";
-import authService from "../auth/auth-services";
+import { useState, useEffect } from "react";
+// import { get, post } from "../http/service";
+// import { useNavigate } from "react-router-dom";
 
-class CreatePet extends React.Component {
-  state = {
-    name: "",
-    age: 0,
-    isFixed: false,
-    typeOfAnimal: "",
-    image: "",
-  };
+function CreatePet(props) {
+  const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("")
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [image, setImage] = useState("");
+  const [preferredLang, setPreferredLang] = useState("");
+  const [changedInfo, setChangedInfo] = useState(false);
+  const [id, setId] = useState("");
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
+  // const navigate = useNavigate();
 
-  preparePetToAdd = (e) => {
+  // useEffect(() => {
+  //   get("/auth/loggedin").then((results) => {
+  //     setUsername(results.data.username);
+  //     setFirstName(results.data.firstName);
+  //     setLastName(results.data.lastName);
+  //     setPreferredLang(results.data.preferredLang);
+  //     setId(results.data._id);
+  //   });
+  // }, []);
+
+  // useEffect(() => {
+  //   if (changedInfo) {
+  //     navigate(`/profile/${id}`);
+  //   }
+  // }, [changedInfo]);
+
+  const saveUserInfo = (e) => {
     e.preventDefault();
-    this.props.createPet(this.state);
-    this.setState({
-      name: "",
-      age: 0,
-      isFixed: false,
-      typeOfAnimal: "",
-    });
+    post("/auth/edit-info", {
+      username,
+      // password,
+      firstName,
+      lastName,
+      preferredLang,
+    })
+      .then((updatedUser) => {
+        props.setUser(updatedUser.data);
+        setChangedInfo(true);
+      })
+      .catch((err) => console.log(err));
   };
 
-  handleFileUpload = (e) => {
-    // console.log("The file to be uploaded is: ", e.target.files[0]);
+  // const goBack = () => {
+  //   navigate(`/profile/${id}`);
+  // };
 
+  // function changePassword() {
+  //   navigate("/auth/change-password");
+  // }
+
+  const handleFileUpload = (e) => {
     const uploadData = new FormData();
 
-    // imageUrl => this name has to be the same as in the model since we pass
-    // req.body to .create() method when creating a new thing in '/api/things/create' POST route
     uploadData.append("imageUrl", e.target.files[0]);
-
-    authService
-      .handleUpload(uploadData)
+    post("/auth/upload-image", {
+      image: uploadData,
+    })
       .then((response) => {
-        console.log("response is: ", response);
-        // after the console.log we can see that response carries 'secure_url' which we can use to update the state
-        this.setState({ image: response.path });
+        console.log("response is::", response);
+        // setImage(response)
       })
-      .catch((err) => console.log("Error while uploading the file: ", err));
+      .catch((err) => console.log("error uploading::", err));
   };
-  render() {
-    return (
-      <div>
-        <h3>Add new pet</h3>
-        <form>
-          <input
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
 
-          <input
-            name="age"
-            value={this.state.age}
-            onChange={this.handleChange}
-          />
-          <input
-            name="isFixed"
-            value={this.state.isFixed}
-            onChange={this.handleChange}
-          />
-          <input
-            name="typeOfAnimal"
-            value={this.state.typeOfAnimal}
-            onChange={this.handleChange}
-          />
+  return (
+    <div>
+      <nav className="navbar nav-pills bg-primary p-2 justify-content-end d-flex justify-content-between">
+        <h1 className="text-white ms-3">InstaHack</h1>
+        <ul className="nav">
+          <li className="m-3">
+            <button onClick={goBack} className="btn btn-primary">
+              Back to Profile
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <h2 className="text-primary m-4 d-flex justify-content-start">
+        Edit your Info:
+      </h2>
 
-          <input name="image" type="file" onChange={this.handleFileUpload} />
-
-          <button onClick={(e) => this.preparePetToAdd(e)}>Create Pet</button>
-        </form>
-      </div>
-    );
-  }
+      <form onSubmit={saveUserInfo}>
+        <div className="m-3">
+          <label htmlFor="username" className="form-label">
+            Username:
+          </label>
+          <input
+            placeholder="Insthacker1"
+            id="username"
+            className="form-control input-sm shadow"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="m-3">
+          <label htmlFor="firstName" className="form-label">
+            First Name:
+          </label>
+          <input
+            placeholder="Insta"
+            className="form-control shadow"
+            id="firstName"
+            onChange={(e) => setFirstName(e.target.value)}
+            value={firstName}
+          />
+        </div>
+        <div className="m-3">
+          <label htmlFor="lastName" className="form-label">
+            Last Name:
+          </label>
+          <input
+            placeholder="Hacker"
+            className="form-control shadow"
+            id="lastName"
+            onChange={(e) => setLastName(e.target.value)}
+            value={lastName}
+          />
+        </div>
+        <div
+          className="m-3"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <label
+            htmlFor="image"
+            className="form-label"
+            style={{ marginRight: "10px" }}
+          >
+            Profile Photo:
+          </label>
+          <input
+            name="image"
+            type="file"
+            onChange={(e) => handleFileUpload(e)}
+          />
+        </div>
+        <div className="m-3">
+          <label htmlFor="preferredLang">Preferred Language:</label>
+          <select
+            id="preferredLang"
+            className="form-select form-select mb-3"
+            aria-label=".form-select-lg example"
+            onChange={(e) => setPreferredLang(e.target.value)}
+          >
+            <option defaultValue>Choose a language</option>
+            <option value="spanish">Spanish</option>
+            <option value="hebrew">Hebrew</option>
+            <option value="french">French</option>
+            <option value="portuguese">Portuguese</option>
+            <option value="chinese">Chinese</option>
+          </select>
+        </div>
+        <button onClick={changePassword} className="btn btn-primary m-3">
+          Change Password
+        </button>
+        <button type="submit" className="btn btn-primary m-3">
+          Save
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default CreatePet;
